7f83107a08547a98e8bf738711d910d5b878a3b9
steve <jedi1014@comcast.net>
2020-10-29 16:48:41 -0400

    Added Branching Class

---
+++ GitBrachingExample/Branching.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitBrachingExample
+{
+    class Branching
+    {
+        public string BranchName { get; set; }
+
+        public Branching()
+        {
+
+        }
+
+        public Branching(string branchName)
+        {
+            BranchName = branchName;
+        }
+    }
+}
