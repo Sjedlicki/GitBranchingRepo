e66f0afce78ffac3eeef2fe618e1e0a2601233e9
steve <jedi1014@comcast.net>
2020-10-29 16:58:37 -0400

    Added Super class

---
+++ GitBrachingExample/Super.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitBrachingExample
+{
+    class Super
+    {
+        public string SuperCool { get; set; }
+
+        public Super()
+        {
+
+        }
+
+        public Super(string superCool)
+        {
+            SuperCool = superCool;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("This is a SUPER class");
+        }
+    }
+}
